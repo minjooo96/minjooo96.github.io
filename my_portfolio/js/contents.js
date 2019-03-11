f084e8e905455566673d28f6f1ef6bd4bb69fe34
minjooo96 <47136112+minjooo96@users.noreply.github.com>
2019-03-12 05:17:47 +0900

    re
    re T.T

---
+++ my_portfolio/js/contents.js
@@ -0,0 +1,23 @@
+var myportfolio = {
+  cube: {
+    pcImg: 'img/portfolio/cube.jpg',
+    taImg: 'img/portfolio/cube.jpg',
+    moImg: 'img/portfolio/cube.jpg',
+    color1: '#aaaa',
+    color2: '#bbbb',
+    color3: '#ccc',
+    title: '[반응형] 큐브엔터테인먼트 리뉴얼',
+    plan: '2018.01.01~2019.01.01(3일)',
+    draft: '2018.01.01~2019.01.01(7일)',
+    coding: '2018.01.01~2019.01.01(8일)',
+    point: '#swiper #flip #scroll',
+    introduce: '것이다 그들은 앞이 긴지라 착목한는 곳이 원대하고 그들은 피가 더운지라 실현에 대한 자신과 용기가 있다 그러므로 그들은 이상의 보배를 능히 품으며 그들의 이상은 아름답고 소담스러운 열매를 맺어 우리 인생을 풍부하게 하는 것이다것이다 그들은 앞이 긴지라 착목한는 곳이 원대하고 그들은 피가 더운지라 실현에 대한 자신과 용기가 있다 그러므로 그들은 이상의 보배를 능히 품으며 그들의 이상은 아름답고 소담스러운 열매를 맺어 우리 인생을 풍부하게 하는 것이다',
+    planPop: 'img/portfolio/cube.pdf',
+    originer: 'http://www.cubeent.co.kr/',
+    renewal: 'https://minjooo96.github.io/cube',
+    code: 'https://github.com/minjooo96/minjooo96.github.io/tree/master/cube',
+  },
+  lottehotel: {
+
+  },
+}
